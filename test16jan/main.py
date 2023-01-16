5c5fdc9f9bc33ef2ea56b8796b0ace35415d1f92
iiiip55 <122780988+iiiip55@users.noreply.github.com>
2023-01-16 17:46:50 +0800

    Add files via upload

---
+++ test16jan/main.py
@@ -0,0 +1,9 @@
+age=int(input))
+print:m=(male=Мужчина)
+print:f=(female=женщитна)
+grade=int(input))
+city=input()
+if age >=10/15 and grade>=m/f  and city == 'Иркутск':
+    print('Доступ разрушен ')
+else:
+    print('Доступ запрещон ')
